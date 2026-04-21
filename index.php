3623f9637abaf8d83081e14afc2216cefb199c6d
freefire21king21-arch <freefire21king21@gmail.com>
2026-04-21 10:28:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,36 @@
 <?php
-
-// By @QGQFQ - تم التعديل والتصحيح
-
-ob_start();
-mkdir('data');
-mkdir('EMIL');
-mkdir('EMILS');
-mkdir('BUY');
-mkdir('assignment');
-mkdir('data/id');
-mkdir('data/txt');
-mkdir('data/api');
 $API_KEY = '8693233767:AAHHXPWFYmyPNIw0RvVsywOXV3ydnqqFtKM';
 define('API_KEY', $API_KEY);
-echo file_get_contents("https://api.telegram.org/bot" . API_KEY . "/setwebhook?url=" . $_SERVER['SERVER_NAME'] . "" . $_SERVER['SCRIPT_NAME']);
-function bot($method, $datas = []) {
-    $amrakl = http_build_query($datas);
-    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method . "?" . $amrakl;
-    $amrakl = file_get_contents($url);
-    return json_decode($amrakl);
-}
-$update = json_decode(file_get_contents('php://input'));
 
-$message = $update->message;
-$chat_id = $message->chat->id;
-$text = $message->text;
-$message_id = $message->message_id;
-$id = $message->from->id;
-if ($update->callback_query) {
-    $id = $update->callback_query->message->chat->id;
-} else {
-    $id = $update->message->chat->id;
+function bot($method, $datas = []) {
+    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
+    if (!empty($datas)) {
+        $url .= "?" . http_build_query($datas);
+    }
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($response, true);
 }
-$user = $message->from->username;
-$first = $message->from->first_name;
-if (isset($update->callback_query)) {
+
+$update = json_decode(file_get_contents('php://input'), true);
+if (!$update) {
+    exit;
+}
+
+if (isset($update['message'])) {
+    $chat_id = $update['message']['chat']['id'];
+    $text = $update['message']['text'] ?? '';
+
+    if ($text === '/start') {
+        bot('sendMessage', [
+            'chat_id' => $chat_id,
+            'text' => "مرحباً! البوت يعمل الآن على Render بنجاح 🚀"
+        ]);
+    }
+}if (isset($update->callback_query)) {
     $chat_id = $update->callback_query->message->chat->id;
     $message_id = $update->callback_query->message->message_id;
     $data = $update->callback_query->data;
